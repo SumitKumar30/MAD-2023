9cd6653fa2a1291bb210ab49792efb20a697f608
Sumit Kumar <mail.sumitkumar30@gmail.com>
2023-02-24 10:29:41 +0530

    Add files via upload

---
+++ unit 3/first_app/lib/main.dart
@@ -0,0 +1,23 @@
+import 'dart:ui';
+
+import 'package:first_app/info_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // return const MaterialApp(
+    //   home: Center(child: SafeArea(child: Text("Hello Flutter!!")))
+    // );
+
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(primarySwatch: Colors.cyan),
+        home: InfoScreen()
+      );
+  }
+}
